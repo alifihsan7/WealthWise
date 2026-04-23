763719e5bb071de3be749dad4cd88fc933196694
yudhasw <yudha.sw2006@gmail.com>
2026-04-23 22:27:06 +0700

    chore: migrate database from Railway to Neon

--- config/database.php
+++ config/database.php
@@ -86,15 +86,8 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST'),
-            'port' => env('DB_PORT', 5432),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'require',
+            'url' => env('DATABASE_URL'),
+            'options' => [], // WAJIB: force array
         ],
 
         'sqlsrv' => [
